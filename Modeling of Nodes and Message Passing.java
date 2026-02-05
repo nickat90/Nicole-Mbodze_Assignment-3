06f852120b7a0d9058f38858e45af4dfaeb83911
nickat90 <148212516+nickat90@users.noreply.github.com>
2026-02-05 19:09:50 +0300

    Update Modeling of Nodes and Message Passing.java
    Updated Node class to include message loss simulation and event handling.

--- Modeling of Nodes and Message Passing.java
+++ Modeling of Nodes and Message Passing.java
@@ -1,21 +1,49 @@
+import java.util.Random;
+
 class Node {
-    String nodeName;
+    String name;
     double latency;
     double throughput;
     double packetLoss;
 
     Node(String name, double latency, double throughput, double packetLoss) {
-        this.nodeName = name;
+        this.name = name;
         this.latency = latency;
         this.throughput = throughput;
         this.packetLoss = packetLoss;
     }
 
     void sendMessage(Node receiver, String message) {
-        System.out.println(nodeName + " sends message to " + receiver.nodeName);
+        Random rand = new Random();
+        if (rand.nextDouble() > packetLoss) {
+            System.out.println(name + " → " + receiver.name + ": " + message);
+        } else {
+            System.out.println("Message lost between " + name + " and " + receiver.name);
+        }
     }
 
     void handleEvent(String event) {
-        System.out.println(nodeName + " handling event: " + event);
+        switch (event) {
+            case "NodeFailure":
+                System.out.println(name + " failure detected. Initiating recovery.");
+                break;
+            case "TransactionCommit":
+                System.out.println(name + " committed transaction successfully.");
+                break;
+            default:
+                System.out.println(name + " processing event: " + event);
+        }
     }
 }
+
+public class DistributedSystem {
+    public static void main(String[] args) {
+        Node edge1 = new Node("Edge1", 12, 500, 0.002);
+        Node core1 = new Node("Core1", 8, 1000, 0.001);
+
+        edge1.sendMessage(core1, "RPC Call");
+        core1.handleEvent("TransactionCommit");
+    }
+}
+
+
