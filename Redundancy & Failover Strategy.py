da05ff3ff3a82f3b8171f8b90c37a73d2c35c370
nickat90 <148212516+nickat90@users.noreply.github.com>
2026-02-05 18:36:30 +0300

    Update Redundancy & Failover Strategy.py
    Refactor failover logic to use get_active_node function.

--- Redundancy & Failover Strategy.py
+++ Redundancy & Failover Strategy.py
@@ -1,7 +1,17 @@
+nodes = {
+    "Core1": "UP",
+    "Core2": "UP"
+}
+
 primary = "Core1"
 backup = "Core2"
 
-def failover(status):
-    if status == "fail":
+def get_active_node():
+    if nodes[primary] == "UP":
+        return primary
+    else:
         return backup
-    return primary
+
+nodes["Core1"] = "DOWN"
+
+print("Active node:", get_active_node())
